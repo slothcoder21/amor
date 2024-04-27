2eec76190a8a492cd58eb44cb8c7fe4c70a2a99d
kinton <kintond15@berkeley.edu>
2024-04-27 16:47:41 -0700

    log in page

--- amor/App.js
+++ amor/App.js
@@ -1,8 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from '/Users/kintonduong/Desktop/amor/amor/pages/LoginScreen.js';
+import HomeScreen from '/Users/kintonduong/Desktop/amor/amor/pages/HomeScreen.js';
 
-export default function App() {
- 
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
-
+export default App;
---
+++ amor/pages/LoginScreen.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+
+function LoginScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+      />
+      <Button
+        title="Log In"
+        onPress={() => navigation.navigate('Home')}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#C9D09A',
+  },
+  input: {
+    width: '100%',
+    marginVertical: 10,
+    padding: 15,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+  }
+});
+
+export default LoginScreen;
