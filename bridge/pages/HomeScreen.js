0a80a94888429ec7b14d500c3e94230c77018ebd
Adrian <adrianlam2003@gmail.com>
2024-04-28 13:44:11 -0700

    Solving merge conflicts

--- bridge/pages/HomeScreen.js
+++ bridge/pages/HomeScreen.js
@@ -13,7 +13,7 @@ function HomeScreen() {
   const addMessage = (message) => {
     setMessages([...messages, message]);
   };
-
+  
   const renderMessages = () => {
     return messages.map((message, index) => (
       <View key={index} style={styles.messageBubble}>
