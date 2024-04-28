6c46d72715f18dd176a690d027b2b9bacf337fdd
kinton <kintond15@berkeley.edu>
2024-04-27 20:13:24 -0700

    installed firebase

--- bridge/App.js
+++ bridge/App.js
@@ -5,7 +5,28 @@ import LoginScreen from './pages/LoginScreen.js';
 import HomeScreen from './pages/HomeScreen.js';
 import SignUpScreen from './pages/SignUpScreen.js';
 import WelcomeScreen from './pages/WelcomeScreen.js';
-//Firebase Imports
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/analytics';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBnRA9y1nPT9Pr2I-VM8bhZ12LXkw6Ln9g",
+  authDomain: "davishacks2024-bridge.firebaseapp.com",
+  projectId: "davishacks2024-bridge",
+  storageBucket: "davishacks2024-bridge.appspot.com",
+  messagingSenderId: "806585146053",
+  appId: "1:806585146053:web:7ff4de014b752fb25e2eac",
+  measurementId: "G-D45Q888MLT"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const analytics = firebase.analytics();
+
 
 
 const Stack = createNativeStackNavigator();
