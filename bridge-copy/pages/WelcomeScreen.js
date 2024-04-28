bab8f05be782441115cee02e7e4d4a82311d5e5c
kinton <kintond15@berkeley.edu>
2024-04-28 09:33:37 -0700

    kdcda

---
+++ bridge-copy/pages/WelcomeScreen.js
@@ -0,0 +1,69 @@
+
+import React from 'react';
+import { View, Text, Button, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import Video from 'react-native-video';
+
+function WelcomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('../assets/curvebridge.png')}
+        style={styles.logo}
+      />
+      <Text style={{fontSize: 70, color: '#9FA54B'}}>
+        b r i d g e
+      </Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Login')}>
+        <Text style={styles.buttonText}>log in</Text>
+      </TouchableOpacity>
+      
+      <Button
+        title="new to bridge? sign up"
+        onPress={() => navigation.navigate('SignUp')}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#C9D09A',
+  },
+  logo: {
+    width: 500, // set the width of the image to make it smaller
+    height: 150, // set the height of the image to make it smaller
+    marginBottom: 20, // add some margin below the image
+    opacity: 0.3,
+  },
+  input: {
+    width: '100%',
+    marginVertical: 10,
+    padding: 15,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+    backgroundColor: '#FFFFFF',
+  },
+  button: {
+    backgroundColor: '#FFFFFF',
+    padding: 10,
+    marginVertical: 10,
+    borderRadius: 25,
+    width: '50%',
+    height: 45,
+    justifyContent: 'center',
+    alignItems: 'center', 
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#9FA54B',
+  }
+});
+
+export default WelcomeScreen;
