a5859a37646bcc0967a1f129bebd67ee62a29216
kinton <kintond15@berkeley.edu>
2024-04-27 23:00:17 -0700

    reverted back to start

--- bridge/App.js
+++ bridge/App.js
@@ -6,26 +6,6 @@ import HomeScreen from './pages/HomeScreen.js';
 import SignUpScreen from './pages/SignUpScreen.js';
 import WelcomeScreen from './pages/WelcomeScreen.js';
 import Config from 'react-native-config';
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
-import 'firebase/analytics';
-
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
-
-const firebaseConfig = {
-  apiKey: Config.FIREBASE_API_KEY,
-  authDomain: Config.FIREBASE_AUTH_DOMAIN,
-  projectId: Config.FIREBASE_PROJECT_ID,
-  storageBucket: Config.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: Config.FIREBASE_MESSAGING_SENDER_ID,
-  appId: Config.FIREBASE_APP_ID,
-  measurementId: Config.FIREBASE_MEASUREMENT_ID
-};
-
-const auth = firebase.auth();
-const firestore = firebase.firestore();
 
 
 
